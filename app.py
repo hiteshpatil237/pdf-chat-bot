c764cc567a6e70f8a6906c42afdf66f9bffc9859
hiteshpatil237 <hiteshpatil237@gmail.com>
2023-12-25 18:54:19 -0500

    Created functions to retrieve texts from pdfs and chunking those docs

--- app.py
+++ app.py
@@ -1,7 +1,30 @@
 import streamlit as st
+from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
 
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+
+    return text
+
+def get_text_chunks(raw_text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+    )
+
+    chunks = text_splitter.split_text(raw_text)
+    return chunks
 
 def main():
+    load_dotenv()
     st.set_page_config(page_title="PDF Chatbot", page_icon=":books:")
 
     st.header("PDF Chatbot :books:")
@@ -9,8 +32,13 @@ def main():
 
     with st.sidebar:
         st.subheader("Your PDFs")
-        st.file_uploader("Upload your PDFs here and click on Process")
-        st.button("Process")
+        pdf_docs = st.file_uploader("Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
+        if st.button("Process"):
+            with st.spinner("Processing"):
+                raw_text = get_pdf_text(pdf_docs)
+
+                text_chunks = get_text_chunks(raw_text)
+                st.write(text_chunks)
 
 if __name__ == '__main__':
     main()
