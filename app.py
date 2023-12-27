dd33df86199e22fb186e96b6d8b6c0e19204370a
hiteshpatil237 <hiteshpatil237@gmail.com>
2023-12-27 14:11:28 -0500

    Completed end to end pdf chat bot

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ from langchain.vectorstores import faiss
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 from langchain.chat_models import ChatOpenAI
+from langchain.llms import huggingface_hub
+from htmlTemplates import css, bot_template, user_template
 
 def get_pdf_text(pdf_docs):
     text = ""
@@ -35,7 +37,8 @@ def get_vector_store(text_chunks):
     return vector_store
 
 def get_conversation_chain(vector_store):
-    llm = ChatOpenAI()
+    # llm = ChatOpenAI()
+    llm = huggingface_hub.HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature": 0.5, "max_length": 512})
     memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
     conversation_chain = ConversationalRetrievalChain.from_llm(
         llm=llm,
@@ -45,12 +48,33 @@ def get_conversation_chain(vector_store):
 
     return conversation_chain
 
+def handle_user_question(user_question):
+    response = st.session_state.conversation({'question': user_question})
+    st.session_state.chat_history = response['chat_history']
+
+    for i, message in enumerate(st.session_state.chat_history):
+        if i % 2 == 0:
+            st.write(user_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
+        else:
+            st.write(bot_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
+
 def main():
     load_dotenv()
     st.set_page_config(page_title="PDF Chatbot", page_icon=":books:")
 
+    st.write(css, unsafe_allow_html=True)
+
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
+    
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = None
+
     st.header("PDF Chatbot :books:")
-    st.text_input("Ask a question about the inputted pdfs: ")
+    user_question = st.text_input("Ask a question about the inputted pdfs: ")
+
+    if user_question:
+        handle_user_question(user_question)
 
     with st.sidebar:
         st.subheader("Your PDFs")
@@ -63,7 +87,7 @@ def main():
 
                 vector_store = get_vector_store(text_chunks)
 
-                conversation = get_conversation_chain(vector_store)
+                st.session_state.conversation = get_conversation_chain(vector_store)
 
 if __name__ == '__main__':
     main()
